22afc618dceab45729724921b270547ace62eda8
Muhammad Hunain <217425888+HunainTahir8007@users.noreply.github.com>
2026-08-04 17:41:57 +0500

    Delete Dataloader langchain directory

--- Dataloader langchain/pdfloader.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader(
-    file_path=r"C:\Users\Hunain Xps\Desktop\Dataloader langchain\Introduction_to_AI_and_ML.pdf"
-)
-
-docs = loader.load()
-
-print(docs[0].page_content)
